5ba4588df22f6ae6fee5defd7fb24372baab6a46
Фаиз <galimow63@gmail.com>
2022-04-22 15:54:23 +0400

    mockedPotsOnlyPercents and mockedPotsOnlyAmounts added

--- test/fixtures/mocked_pots.dart
+++ test/fixtures/mocked_pots.dart
@@ -1,7 +1,19 @@
 import 'package:potty/features/potties_manager/domain/entities/pot.dart';
 
-final mockedPots = [
+final mockedPotsFull = [
   Pot(id: '1', name: 'name1', percent: 10, amount: 100),
   Pot(id: '2', name: 'name2', percent: 20, amount: 200),
   Pot(id: '3', name: 'name3', percent: 40, amount: 400),
+];
+
+final mockedPotsOnlyPercents = [
+  Pot(id: '1', name: 'name1', percent: 10),
+  Pot(id: '2', name: 'name2', percent: 20),
+  Pot(id: '3', name: 'name3', percent: 40),
+];
+
+final mockedPotsOnlyAmounts = [
+  Pot(id: '1', name: 'name1', amount: 100),
+  Pot(id: '2', name: 'name2', amount: 200),
+  Pot(id: '3', name: 'name3', amount: 400),
 ];

1c61d1adacf4586184520f44936b4afaf286c314
Фаиз <galimow63@gmail.com>
2022-04-22 15:58:09 +0400

    isAmountFixed assigned in mockPots

--- test/fixtures/mocked_pots.dart
+++ test/fixtures/mocked_pots.dart
@@ -7,13 +7,13 @@ final mockedPotsFull = [
 ];
 
 final mockedPotsOnlyPercents = [
-  Pot(id: '1', name: 'name1', percent: 10),
-  Pot(id: '2', name: 'name2', percent: 20),
-  Pot(id: '3', name: 'name3', percent: 40),
+  Pot(id: '1', name: 'name1', percent: 10, isAmountFixed: false),
+  Pot(id: '2', name: 'name2', percent: 20, isAmountFixed: false),
+  Pot(id: '3', name: 'name3', percent: 40, isAmountFixed: false),
 ];
 
 final mockedPotsOnlyAmounts = [
-  Pot(id: '1', name: 'name1', amount: 100),
-  Pot(id: '2', name: 'name2', amount: 200),
-  Pot(id: '3', name: 'name3', amount: 400),
+  Pot(id: '1', name: 'name1', amount: 100, isAmountFixed: true),
+  Pot(id: '2', name: 'name2', amount: 200, isAmountFixed: true),
+  Pot(id: '3', name: 'name3', amount: 400, isAmountFixed: true),
 ];

8f7951198f2878858bdcd4f17f95f13e1c6069e5
Фаиз <galimow63@gmail.com>
2022-04-26 22:20:01 +0400

    PotsRepositoryImpl: addPot

--- lib/features/potties_manager/data/repositories/pots_repository_impl.dart
+++ lib/features/potties_manager/data/repositories/pots_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:potty/features/potties_manager/data/datasources/local_datasource.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../../domain/entities/pot.dart';
@@ -7,13 +8,18 @@ import '../../domain/repositories/pots_repository.dart';
 import '../../domain/usecases/sort_pot.dart';
 
 class PotsRepositoryImpl implements IPotsRepository {
+  late ILocalDatasource localDatasource;
+
+  PotsRepositoryImpl({required this.localDatasource});
+
   @override
   late List<PotSet> potsets;
 
   @override
-  Future<void> addPot(String potSetId, Pot newPot) {
-    // TODO: implement addPot
-    throw UnimplementedError();
+  Future<void> addPot(String potSetId, Pot newPot) async {
+    final currentPotSet = _definePotSet(potSetId);
+    currentPotSet.addPot(newPot: newPot);
+    await localDatasource.saveToMemory(currentPotSet);
   }
 
   @override
@@ -62,5 +68,9 @@ class PotsRepositoryImpl implements IPotsRepository {
   Future<void> updatePot(String potSetId, String potId, Pot newPot) {
     // TODO: implement updatePot
     throw UnimplementedError();
+  }
+
+  PotSet _definePotSet(String potSetId) {
+    return potsets.firstWhere((potSet) => potSet.id == potSetId);
   }
 }

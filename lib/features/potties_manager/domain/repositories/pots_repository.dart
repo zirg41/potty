f1b79f3fc13869dccfcfe83070bbe5beb72ae954
Фаиз <galimow63@gmail.com>
2022-04-26 19:57:48 +0400

    IPotsRepository abstract class added

--- lib/features/potties_manager/domain/repositories/pots_repository.dart
+++ lib/features/potties_manager/domain/repositories/pots_repository.dart
@@ -3,12 +3,18 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failure.dart';
 import '../entities/pot.dart';
 import '../entities/pot_set.dart';
+import '../usecases/sort_pot.dart';
 
 abstract class IPotsRepository {
+  late List<PotSet> potsets;
+
   Future<Either<Failure, List<PotSet>>> getAllPots();
-  Future<void> addPot(String potSetId, Pot pot);
-  Future<void> updatePot(String potSetId, Pot pot);
-  Future<void> deletePot(String potSetId, String potId);
-  Future<void> createPotSet(PotSet potSet);
-  // Future<void> calculate(String potSetId);
+  Future<void> addPotSet(PotSet potSet);
+  Future<void> addPot(String potSetId, Pot newPot);
+  Future<void> setSorting(String potSetId, ISortPot sortingLogic);
+  Future<void> updatePot(String potSetId, String potId, Pot newPot);
+  Future<void> deletePot(String potSetId, Pot newPot);
+  Future<void> deletePotSet(String potSetId);
+  Future<void> changePotSetName(String newName);
+  Future<void> changePotSetIncome(double newIncome);
 }

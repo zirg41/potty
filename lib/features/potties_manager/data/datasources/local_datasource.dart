7ed0e44c3859e12d285bd9182f7a69de2541dd9f
Фаиз <galimow63@gmail.com>
2022-04-26 20:26:01 +0400

    ILocalDatasource finished

--- lib/features/potties_manager/data/datasources/local_datasource.dart
+++ lib/features/potties_manager/data/datasources/local_datasource.dart
@@ -3,6 +3,6 @@ import 'package:potty/core/errors/failure.dart';
 import 'package:potty/features/potties_manager/domain/entities/pot_set.dart';
 
 abstract class ILocalDatasource {
-  Future<void> saveToMemory(PotSetModel potset);
+  Future<void> saveToMemory(PotSet potset);
   Future<Either<Failure, List<PotSet>>> getFromMemory();
 }

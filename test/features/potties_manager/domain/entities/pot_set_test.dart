468f368a61ba8f170b6ac0d11e6f1945cb93b9cd
Фаиз <galimow63@gmail.com>
2022-04-26 12:38:49 +0400

    adding and calculating test added

--- test/features/potties_manager/domain/entities/pot_set_test.dart
+++ test/features/potties_manager/domain/entities/pot_set_test.dart
@@ -1,8 +1,48 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:potty/features/potties_manager/domain/entities/pot.dart';
+import 'package:potty/features/potties_manager/domain/entities/pot_set.dart';
+
+final pot1 = Pot(id: '1', name: 'pot1', percent: 10, isAmountFixed: false);
+final pot2 = Pot(id: '2', name: 'pot2', amount: 150, isAmountFixed: true);
+final pot3 = Pot(id: '3', name: 'pot3', amount: 250, isAmountFixed: true);
+
+final potList = [pot1, pot2, pot3];
 
 void main() {
+  late PotSet potset;
+
+  setUp(
+    () {
+      potset = PotSet(
+        id: 'test_id',
+        income: 1000,
+        name: 'test name',
+        createdDate: DateTime.utc(2022),
+        pots: [],
+      );
+    },
+  );
+
   group(
     'PotSet',
-    () {},
+    () {
+      test(
+        "should add new Pot, calculate pot and remains",
+        () async {
+          // act
+          potset.addPot(newPot: pot1);
+          // assert
+          expect(potset.pots[0], pot1);
+          expect(potset.unallocatedPercent, 90);
+          expect(potset.unallocatedBalance, 900);
+          // act
+          potset.addPot(newPot: pot2);
+          // assert
+          expect(potset.pots[1], pot2);
+          expect(potset.unallocatedPercent, 75);
+          expect(potset.unallocatedBalance, 750);
+        },
+      );
+    },
   );
 }

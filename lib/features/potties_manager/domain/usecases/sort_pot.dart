25647c3fccf69dbcc5b855cf0b4b0715be40a08b
Фаиз <galimow63@gmail.com>
2022-04-26 11:34:25 +0400

    SortLowToHigh class added

--- lib/features/potties_manager/domain/usecases/sort_pot.dart
+++ lib/features/potties_manager/domain/usecases/sort_pot.dart
@@ -1,5 +1,15 @@
+import 'dart:isolate';
+
 import 'package:potty/features/potties_manager/domain/entities/pot.dart';
 
 abstract class ISortPot {
   List<Pot> sortPots(List<Pot> potsToSort);
+}
+
+class SortLowToHigh implements ISortPot {
+  @override
+  List<Pot> sortPots(List<Pot> potsToSort) {
+    potsToSort.sort((potA, potB) => potA.percent!.compareTo(potB.percent!));
+    return potsToSort;
+  }
 }

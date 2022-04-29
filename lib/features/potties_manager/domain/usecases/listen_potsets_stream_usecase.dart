0a9d7afe949e61ea6160ea23091024237284f49a
Фаиз <galimow63@gmail.com>
2022-04-29 20:12:53 +0400

    ListenPotSetsStreamUseCase added

---
+++ lib/features/potties_manager/domain/usecases/listen_potsets_stream_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entities/pot_set.dart';
+import '../repositories/i_pots_repository.dart';
+
+/// Unusable usecase. Better listen directly from repository
+class ListenPotSetsStreamUseCase {
+  IPotsRepository potsRepository;
+
+  ListenPotSetsStreamUseCase({
+    required this.potsRepository,
+  });
+
+  Stream<Either<Failure, List<PotSet>>> call() async* {
+    yield* potsRepository.getAllPots();
+  }
+}

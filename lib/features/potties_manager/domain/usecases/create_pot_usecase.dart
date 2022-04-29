e5687dcf416cf8ed1ba5111a8e07750c6915dd33
Фаиз <galimow63@gmail.com>
2022-04-29 19:49:57 +0400

    CreatePotUseCase added

---
+++ lib/features/potties_manager/domain/usecases/create_pot_usecase.dart
@@ -0,0 +1,38 @@
+import '../../../../core/util/id_generator.dart';
+import '../../../../core/util/pot_creator.dart';
+import '../entities/pot.dart';
+import '../repositories/i_pots_repository.dart';
+
+class CreatePotUseCase {
+  IPotsRepository potsRepository;
+  IDGenerator idGenerator;
+  late IPotCreator potCreator;
+
+  CreatePotUseCase({
+    required this.potsRepository,
+    required this.idGenerator,
+    potCreator = const EmptyPot(),
+  });
+
+  Future<void> call({
+    required String potSetId,
+    String name = 'default name',
+    bool? isAmountFixed,
+    double? amount,
+    double? percent,
+    IPotCreator? potCreator,
+  }) async {
+    Pot newPot = Pot(
+      id: idGenerator.generateID(),
+      name: name,
+      isAmountFixed: isAmountFixed,
+      amount: amount,
+      percent: percent,
+    );
+    Pot? manualPot = potCreator?.call();
+    await potsRepository.addPot(
+        potSetId,
+        manualPot ?? newPot
+          ..id = idGenerator.generateID());
+  }
+}

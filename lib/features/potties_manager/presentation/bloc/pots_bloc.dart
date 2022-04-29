a406035558c33683395c5a35303d41746726a07d
Фаиз <galimow63@gmail.com>
2022-04-29 20:32:11 +0400

    dependecies added to PotsBloc

--- lib/features/potties_manager/presentation/bloc/pots_bloc.dart
+++ lib/features/potties_manager/presentation/bloc/pots_bloc.dart
@@ -1,21 +1,44 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:potty/features/potties_manager/domain/entities/pot_set.dart';
-import 'package:potty/features/potties_manager/domain/repositories/i_pots_repository.dart';
+
+import '../../domain/entities/pot_set.dart';
+import '../../domain/usecases/create_pot_set_usecase.dart';
+import '../../domain/usecases/create_pot_usecase.dart';
+import '../../domain/usecases/delete_pot_set_usecase.dart';
+import '../../domain/usecases/delete_pot_usecase.dart';
+import '../../domain/usecases/edit_pot_usecase.dart';
+import '../../domain/usecases/edit_potset_usecase.dart';
+import '../../domain/usecases/listen_potsets_stream_usecase.dart';
+import '../../domain/usecases/set_sorting_usecase.dart';
 
 part 'pots_event.dart';
 part 'pots_state.dart';
 
 class PotsBloc extends Bloc<PotsEvent, PotsState> {
-  IPotsRepository potsRepository;
+  final CreatePotUseCase createPotUseCase;
+  final EditPotUseCase editPotUseCase;
+  final DeletePotUseCase deletePotUseCase;
+  final CreatePotSetUseCase createPotSetUseCase;
+  final EditPotSetUseCase editPotSetUseCase;
+  final DeletePotSetUseCase deletePotSetUseCase;
+  final ListenPotSetsStreamUseCase listenPotSetsStreamUseCase;
+  final SetSortingUseCase setSortingUseCase;
 
   PotsBloc({
-    required this.potsRepository,
+    required this.createPotUseCase,
+    required this.createPotSetUseCase,
+    required this.deletePotSetUseCase,
+    required this.deletePotUseCase,
+    required this.editPotUseCase,
+    required this.editPotSetUseCase,
+    required this.listenPotSetsStreamUseCase,
+    required this.setSortingUseCase,
   }) : super(PotsInitial()) {
     on<PotsEvent>((event, emit) {});
+
     on<GetPotsEvent>((event, emit) {
       emit(LoadingState());
-      final potsStream = potsRepository.getAllPots();
+      final potsStream = listenPotSetsStreamUseCase();
       potsStream.listen((event) {
         event.fold(
           (failure) => GetPotsErrorState(),

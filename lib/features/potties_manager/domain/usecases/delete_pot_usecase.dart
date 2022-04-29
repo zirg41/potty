9cf817ec91ba7abcac8ae6eab41527dbf9460205
Фаиз <galimow63@gmail.com>
2022-04-29 20:04:47 +0400

    DeletePotUseCase added

---
+++ lib/features/potties_manager/domain/usecases/delete_pot_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:potty/features/potties_manager/domain/repositories/i_pots_repository.dart';
+
+class DeletePotUseCase {
+  IPotsRepository potsRepository;
+
+  DeletePotUseCase({
+    required this.potsRepository,
+  });
+
+  Future<void> call({
+    required String potSetId,
+    required String potIdToDelete,
+  }) async {
+    await potsRepository.deletePot(potSetId, potIdToDelete);
+  }
+}

e5b309e76f391eedb4102ddaf734709e175e65d5
Фаиз <galimow63@gmail.com>
2022-04-29 10:32:41 +0400

    PotsRepositoryImpl add pot and potset test

--- test/features/potties_manager/data/repositories/pots_repository_impl_test.dart
+++ test/features/potties_manager/data/repositories/pots_repository_impl_test.dart
@@ -1,45 +1,85 @@
-import 'dart:ffi';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:potty/features/potties_manager/data/datasources/i_local_datasource.dart';
+import 'package:hive/hive.dart';
+import 'package:potty/features/potties_manager/data/datasources/local_datasource_impl.dart';
+import 'package:potty/features/potties_manager/data/models/pot_model.dart';
+import 'package:potty/features/potties_manager/data/models/pot_set_model.dart';
+import 'package:potty/features/potties_manager/data/models/sorting_logic_model.dart';
 import 'package:potty/features/potties_manager/data/repositories/pots_repository_impl.dart';
 import 'package:potty/features/potties_manager/domain/entities/pot_set.dart';
 
 import '../../domain/entities/pot_set_test.dart';
 
-class MockLocalDatasource extends Mock implements ILocalDatasource {}
-
 final mockPotSet = PotSet(
-  id: 'potSetid',
+  id: 'id1',
   income: 100,
-  name: 'name',
+  name: 'PotSetReposTest',
   pots: [],
   createdDate: DateTime.now(),
-);
-void main() {
+)..changeIncome(newIncome: 1000);
+
+void main() async {
   late PotsRepositoryImpl potsRepository;
-  late MockLocalDatasource mockLocalDatasource;
+  late LocalHiveDatasourceImpl localHiveDatasourceImpl;
+  late List<PotSet> listOfPotSets;
+
+  WidgetsFlutterBinding.ensureInitialized();
+  Hive.init('D:\\database');
+  Hive.registerAdapter(SortingLogicModelAdapter());
+  Hive.registerAdapter(PotSetHiveModelAdapter());
+  Hive.registerAdapter(PotHiveModelAdapter());
+
+  final potSetBox = await Hive.openBox<PotSetHiveModel>('potSetModels');
+  localHiveDatasourceImpl = LocalHiveDatasourceImpl(potSetBox);
+  potsRepository = PotsRepositoryImpl(localDatasource: localHiveDatasourceImpl);
 
   setUp(() {
-    mockLocalDatasource = MockLocalDatasource();
-    potsRepository = PotsRepositoryImpl(localDatasource: mockLocalDatasource);
-    potsRepository.potsets = [mockPotSet];
+    listOfPotSets = [];
   });
 
   group(
     'PotsRepositoryImpl',
     () {
       test(
-        "should invoke saveToMemory method while adding new Pot",
+        "should save potset to memory",
         () async {
           // arrange
-          //when(mockLocalDatasource.saveToMemory(mockPotSet))
-          //  .thenAnswer((realInvocation) => Future.value(Future<void>));
+          localHiveDatasourceImpl.getFromMemory().listen((event) {
+            event.fold(
+              (failure) {
+                print(failure.toString());
+                return [];
+              },
+              (listofpotset) {
+                print(
+                  '[LOCAL DB]: There is ${listofpotset.length} potSets in memory with hashcode ${listofpotset.hashCode}',
+                );
+                listOfPotSets = listofpotset;
+              },
+            );
+          });
           // act
-          //potsRepository.addPot("potSetid", pot1);
+          await potsRepository.addPotSet(mockPotSet);
           // assert
-          // TODO verify(mockLocalDatasource.saveToMemory(potsRepository.potsets[0]));
+
+          print(
+              "[Listener]: There is ${listOfPotSets.length} potSets in fetched list with hashcode ${listOfPotSets.hashCode}");
+
+          expect(listOfPotSets, [mockPotSet]);
+        },
+      );
+      test(
+        "should add new pot to existing potset",
+        () async {
+          // act
+          await potsRepository.addPot(mockPotSet.id, pot1);
+          // assert
+          final modifiedPotSet = mockPotSet..pots = [pot1];
+
+          print(
+              "[Listener]: There is ${listOfPotSets.length} potSets in fetched list with hashcode ${listOfPotSets.hashCode}");
+
+          expect(listOfPotSets, [modifiedPotSet]);
         },
       );
     },

73eef1529c0652ee04a5b99483b50fe105d6f2e0
Фаиз <galimow63@gmail.com>
2022-04-29 23:50:28 +0400

    InputConverter added

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:dartz/dartz.dart';
+import 'package:potty/core/errors/failure.dart';
+
+class InputConverter {
+  Either<Failure, double> stringToUnsignedInteger(String str) {
+    try {
+      final parsedAmount = double.parse(str);
+      if (parsedAmount < 0) {
+        throw const FormatException();
+      }
+      return Right(parsedAmount);
+    } on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {}
+
+const String CACHE_FAILURE_MESSAGE = 'Ошибка чтения с памяти телефона';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Некорректная величина. Значение должно быть положительным числом';

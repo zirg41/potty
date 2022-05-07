1ab448b797c24a09ef8bb5e070727ad919d20aa7
Фаиз <galimow63@gmail.com>
2022-05-08 00:12:37 +0400

    on<PotsWatcherGetAllPotsEvent> event handler implmented

--- lib/features/potties_manager/presentation/bloc/pots_watcher/pots_watcher_bloc.dart
+++ lib/features/potties_manager/presentation/bloc/pots_watcher/pots_watcher_bloc.dart
@@ -1,13 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:potty/features/potties_manager/domain/entities/pot_set.dart';
+import 'package:potty/features/potties_manager/domain/usecases/listen_potsets_stream_usecase.dart';
 part 'pots_watcher_event.dart';
 part 'pots_watcher_state.dart';
 
 class PotsWatcherBloc extends Bloc<PotsWatcherEvent, PotsWatcherState> {
-  PotsWatcherBloc() : super(PotsWatcherInitial()) {
-    on<PotsWatcherEvent>((event, emit) {
-      // TODO: implement event handler
+  ListenPotSetsStreamUseCase listenPotSetsStreamUseCase;
+
+  PotsWatcherBloc(this.listenPotSetsStreamUseCase)
+      : super(PotsWatcherInitial()) {
+    on<PotsWatcherGetAllPotsEvent>((event, emit) {
+      emit(const PotsWatcherLoadingState());
+
+      final potsStream = listenPotSetsStreamUseCase();
+
+      potsStream.listen((event) {
+        event.fold(
+          (failure) async => emit(const PotsWatcherLoadingError()),
+          (potSets) async {
+            print('got potsstream in bloc ${potSets.length}');
+
+            emit(PotsWatcherLoadedState(potSets));
+          },
+        );
+      });
     });
   }
 }

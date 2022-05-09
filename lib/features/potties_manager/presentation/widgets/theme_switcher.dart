f75daefb5cf3c15c5ba540f752ac69266b0c0d73
Фаиз <galimow63@gmail.com>
2022-05-09 11:19:42 +0400

    ThemeSwitcherWidget added

---
+++ lib/features/potties_manager/presentation/widgets/theme_switcher.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../dependency_injection.dart';
+import '../../../../global/theme/app_themes.dart';
+import '../../../../global/theme/bloc/theme_bloc.dart';
+
+class ThemeSwitcherWidget extends StatelessWidget {
+  final bool toggled;
+
+  const ThemeSwitcherWidget({
+    required this.toggled,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => sl<ThemeBloc>(),
+      child: SwitchListTile(
+        title: const Text('Темная тема'),
+        value: toggled,
+        onChanged: (value) {
+          final AppTheme appTheme;
+          value
+              ? appTheme = AppTheme.darkTheme
+              : appTheme = AppTheme.lightTheme;
+          BlocProvider.of<ThemeBloc>(context).add(ThemeChanged(appTheme));
+        },
+      ),
+    );
+  }
+}

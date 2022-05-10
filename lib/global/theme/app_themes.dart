155ccfcd89e5c74e8de5e71a45975025cbf419b9
Фаиз <galimow63@gmail.com>
2022-05-10 19:01:37 +0400

    google fonts added + styling

--- lib/global/theme/app_themes.dart
+++ lib/global/theme/app_themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 enum AppTheme {
   lightTheme,
@@ -9,9 +10,15 @@ final appThemeData = {
   AppTheme.lightTheme: ThemeData(
     brightness: Brightness.light,
     primaryColor: Colors.indigo,
+    textTheme: TextTheme(
+      titleLarge: GoogleFonts.lato(fontSize: 25),
+    ),
   ),
   AppTheme.darkTheme: ThemeData(
     brightness: Brightness.dark,
     primaryColor: Colors.indigo[800],
+    textTheme: TextTheme(
+      titleLarge: GoogleFonts.lato(fontSize: 25),
+    ),
   )
 };

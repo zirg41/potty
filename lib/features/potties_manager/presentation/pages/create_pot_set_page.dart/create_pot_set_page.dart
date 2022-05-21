183db85ddb6ecbf9d7262efa7b9fc50a6c9ae204
Фаиз <galimow63@gmail.com>
2022-05-21 23:57:08 +0400

    CreatePotSetBody modified

--- lib/features/potties_manager/presentation/pages/create_pot_set_page.dart/create_pot_set_page.dart
+++ lib/features/potties_manager/presentation/pages/create_pot_set_page.dart/create_pot_set_page.dart
@@ -4,36 +4,172 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../bloc/pots_actor/pots_bloc.dart';
 
 class CreatePotSetBody extends StatefulWidget {
-  CreatePotSetBody({Key? key}) : super(key: key);
+  const CreatePotSetBody({Key? key}) : super(key: key);
 
   @override
   State<CreatePotSetBody> createState() => _CreatePotSetBodyState();
 }
 
 class _CreatePotSetBodyState extends State<CreatePotSetBody> {
+  final _formKey = GlobalKey<FormState>();
   final _incomeController = TextEditingController();
   final _nameController = TextEditingController();
   final _incomeAmountFocusNode = FocusNode();
 
   String _previousIncomeValue = '';
-  String _previousNameValue = '';
+
+  void _saveForm(BuildContext context) {
+    BlocProvider.of<PotsBloc>(context).add(
+      CreatePotSetEvent(
+        name: _nameController.text,
+        income: _incomeController.text,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _incomeController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final themeDataColorScheme = Theme.of(context).colorScheme;
+    final ctxTheme = Theme.of(context);
+    final mediaQuery = MediaQuery.of(context);
+
+    final textFieldBorderRadius = BorderRadius.circular(10);
+    final _enabledBorder = OutlineInputBorder(
+        borderSide: BorderSide(width: 1.5, color: themeDataColorScheme.outline),
+        borderRadius: textFieldBorderRadius);
+    final _focusedBorder = OutlineInputBorder(
+        borderSide:
+            BorderSide(width: 1.5, color: themeDataColorScheme.onSurface),
+        borderRadius: textFieldBorderRadius);
+    final _errorBorder = OutlineInputBorder(
+        borderSide: BorderSide(width: 1.5, color: themeDataColorScheme.error),
+        borderRadius: textFieldBorderRadius);
+
     return BlocListener<PotsBloc, PotsState>(
       listener: (context, state) => _stateHandler(context, state),
       child: Scaffold(
+        backgroundColor: Colors.transparent,
         body: Stack(
-          children: [],
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            Column(
+              children: [
+                const SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      constraints: BoxConstraints(
+                          maxHeight: mediaQuery.size.height * 0.35,
+                          maxWidth: mediaQuery.size.width * 0.7),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        color: ctxTheme.colorScheme.surface,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: LayoutBuilder(
+                            builder: (context, constraints) {
+                              return Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: [
+                                    // ! PotSet's name Text Field
+                                    TextFormField(
+                                      controller: _nameController,
+                                      autocorrect: false,
+                                      autofocus: true,
+                                      keyboardType: TextInputType.name,
+                                      textInputAction: TextInputAction.next,
+                                      onFieldSubmitted: (value) {
+                                        _incomeAmountFocusNode.requestFocus();
+                                      },
+                                      textCapitalization:
+                                          TextCapitalization.sentences,
+                                      decoration: InputDecoration(
+                                        hintText: 'Наименование',
+                                        enabledBorder: _enabledBorder,
+                                        focusedBorder: _focusedBorder,
+                                        errorBorder: _errorBorder,
+                                        suffixIcon: IconButton(
+                                          onPressed: _nameController.clear,
+                                          color: themeDataColorScheme.outline,
+                                          focusColor:
+                                              themeDataColorScheme.onSurface,
+                                          icon: const Icon(Icons.clear),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    // ! Income text field
+                                    TextFormField(
+                                      controller: _incomeController,
+                                      keyboardType: TextInputType.number,
+                                      focusNode: _incomeAmountFocusNode,
+                                      onFieldSubmitted: (_) =>
+                                          _saveForm(context),
+                                      decoration: InputDecoration(
+                                        enabledBorder: _enabledBorder,
+                                        focusedBorder: _focusedBorder,
+                                        errorBorder: _errorBorder,
+                                        hintText: 'Доход, RUB',
+                                        suffixIcon: IconButton(
+                                          onPressed: () =>
+                                              _incomeController.clear(),
+                                          color: themeDataColorScheme.outline,
+                                          focusColor:
+                                              themeDataColorScheme.onSurface,
+                                          icon: const Icon(Icons.clear),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 15),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        TextButton(
+                                            onPressed: () =>
+                                                Navigator.of(context).pop(),
+                                            child: const Text('Отменить')),
+                                        ElevatedButton(
+                                          child: const Text('Сохранить'),
+                                          onPressed: () => _saveForm(context),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 
   void _stateHandler(BuildContext context, PotsState state) {
-    if (state is PercentOrAmountInputErrorState) {
+    if (state is IncomeInputErrorState) {
       _incomeController.text = _previousIncomeValue;
-      _nameController.text = _previousNameValue;
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(

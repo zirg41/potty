ce02592bea662d751924ec3dc4d7ced98b12ff4d
Фаиз <galimow63@gmail.com>
2022-05-22 15:27:34 +0400

    PotSetAppBar now added UserEditingEitherNameOrIncomeOfPotSetEvent while editing

--- lib/features/potties_manager/presentation/pages/concrete_pot_set_overview_page/widgets/potset_appbar.dart
+++ lib/features/potties_manager/presentation/pages/concrete_pot_set_overview_page/widgets/potset_appbar.dart
@@ -18,43 +18,57 @@ class PotSetAppBar extends StatefulWidget {
 
 class _PotSetAppBarState extends State<PotSetAppBar> {
   bool isEditing = false;
-  final controller = TextEditingController();
+  final _potSetNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final contextTheme = Theme.of(context);
 
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isEditing = true;
-        });
-        controller.selection =
-            TextSelection(baseOffset: 0, extentOffset: controller.text.length);
+    return BlocListener<PotsBloc, PotsState>(
+      listener: (context, state) {
+        if (state is PotsChangedSuccesfullyState) {
+          setState(() {
+            isEditing = false;
+          });
+        }
       },
-      child: isEditing
-          ? TextField(
-              controller: controller,
-              onSubmitted: (value) {
-                BlocProvider.of<PotsBloc>(context).add(EditPotSetNameEvent(
-                    potSetId: widget.potSetId, name: value));
-                isEditing = false;
-              },
-              autofocus: true,
-              style: contextTheme.textTheme.titleLarge,
-              decoration: const InputDecoration(border: InputBorder.none),
-            )
-          : BlocBuilder<PotsWatcherBloc, PotsWatcherState>(
-              bloc: BlocProvider.of<PotsWatcherBloc>(context),
-              builder: (context, state) {
-                if (state is PotSetsLoadedState) {
-                  final potSet = state.getPotSetById(potSetId: widget.potSetId);
-                  controller.text = potSet.name;
-                  return Text(potSet.name);
-                }
-                return const Text(NO_STATE_ERROR);
-              },
-            ),
+      child: GestureDetector(
+        onTap: () {
+          BlocProvider.of<PotsBloc>(context)
+              .add(const UserEditingEitherNameOrIncomeOfPotSetEvent());
+          setState(() {
+            isEditing = true;
+          });
+          _potSetNameController.selection = TextSelection(
+            baseOffset: 0,
+            extentOffset: _potSetNameController.text.length,
+          );
+        },
+        child: isEditing
+            ? TextField(
+                controller: _potSetNameController,
+                onSubmitted: (value) {
+                  BlocProvider.of<PotsBloc>(context).add(EditPotSetNameEvent(
+                      potSetId: widget.potSetId, name: value));
+                  isEditing = false;
+                },
+                autofocus: true,
+                style: contextTheme.textTheme.titleLarge,
+                decoration: const InputDecoration(border: InputBorder.none),
+              )
+            : BlocBuilder<PotsWatcherBloc, PotsWatcherState>(
+                bloc: BlocProvider.of<PotsWatcherBloc>(context),
+                builder: (context, state) {
+                  if (state is PotSetsLoadedState) {
+                    final potSet =
+                        state.getPotSetById(potSetId: widget.potSetId);
+                    _potSetNameController.text = potSet.name;
+                    return Text(potSet.name);
+                  }
+                  return const Text(NO_STATE_ERROR);
+                },
+              ),
+      ),
     );
   }
 }

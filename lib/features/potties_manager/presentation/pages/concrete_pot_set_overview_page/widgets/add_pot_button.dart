ac632828a6e76c4e960d05ae237ecdaa342c3051
Фаиз <galimow63@gmail.com>
2022-05-22 15:36:31 +0400

    AddPotButton now is sensetive to UserEditingEitherNameOrIncomeOfPotSetState

--- lib/features/potties_manager/presentation/pages/concrete_pot_set_overview_page/widgets/add_pot_button.dart
+++ lib/features/potties_manager/presentation/pages/concrete_pot_set_overview_page/widgets/add_pot_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../bloc/pots_actor/pots_bloc.dart';
 import '../../edit_pot_page/edit_pot_widget.dart';
 import '../../pot_sets_overview_page/widgets/pot_set_item.dart';
 
@@ -12,26 +14,44 @@ class AddPotButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isEnabled = true;
+
     final contextTheme = Theme.of(context);
-    return GestureDetector(
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (ctx) {
-            return EditPotWidget(
-              potSetId: potSetId,
-            );
-          },
-        );
+    return BlocListener<PotsBloc, PotsState>(
+      listener: (context, state) {
+        if (state is UserEditingEitherNameOrIncomeOfPotSetState) {
+          isEnabled = false;
+        }
+        if (state is PotsChangedSuccesfullyState) {
+          isEnabled = true;
+        }
       },
-      child: Padding(
-        padding: itemsPadding,
-        child: Card(
-          color: contextTheme.colorScheme.surface,
-          child: const SizedBox(
-            height: 50,
-            child: Center(
-              child: Icon(Icons.add),
+      child: GestureDetector(
+        onTap: () {
+          if (isEnabled) {
+            showDialog(
+              context: context,
+              builder: (ctx) {
+                return EditPotWidget(
+                  potSetId: potSetId,
+                );
+              },
+            );
+          } else {
+            FocusManager.instance.primaryFocus?.unfocus();
+            BlocProvider.of<PotsBloc>(context)
+                .add(const PotsChangedSuccesfullyEvent());
+          }
+        },
+        child: Padding(
+          padding: itemsPadding,
+          child: Card(
+            color: contextTheme.colorScheme.surface,
+            child: const SizedBox(
+              height: 50,
+              child: Center(
+                child: Icon(Icons.add),
+              ),
             ),
           ),
         ),
